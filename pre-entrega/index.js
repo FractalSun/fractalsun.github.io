8b65ae8d37526d6852c550851271fc9fb67975b7
Gaston M <gastonux@gmail.com>
2026-05-28 03:14:37 -0300

    Se termina el GET para un producto en especifico y se termina el case POST

--- pre-entrega/index.js
+++ pre-entrega/index.js
@@ -7,7 +7,7 @@ const argumentos_validos = ["GET", "POST", "PUT", "DELETE"]
 
 console.log(argumentos) 
 
-async function programa_principal(argumentos) {
+async function programa_principal(argumentos = []) {
     if (!argumentos[0] in argumentos_validos){
         console.log("Comando incorrecto")
         return
@@ -26,11 +26,57 @@ async function programa_principal(argumentos) {
                     const data = await response.json()
                     data.forEach(element => {
                         console.log(element)
+                    });
+                    break
+                }catch(error){
+                    console.log(error)
+                    break
+                } 
+            }else if (argumentos[1].includes("/") && argumentos[1].includes("products")) {
+                let id_sin_separar = argumentos[1].split("/")
+                try{
+                    const id = parseInt(id_sin_separar[1])
+                    const response = await fetch(`${url_API}/products/${id}`,{
+                        method: "GET"
                     })
+                    if (response.status !== 200){
+                        throw new Error("Error en la solicitud")
+                        break;
+                    }
+                    const data = await response.json()
+                    console.log(data)
+                    break;
                 }catch(error){
                     console.log(error)
                     break
                 }
+            }else{
+                console.log("Comando incorrecto")
+                break
+            }
+        case "POST":
+            if(argumentos.length == 5 && argumentos[1] == "products"){
+                const [ , , nombre, precio, categoria] = argumentos
+                const response = await fetch(`${url_API}/products`,{
+                    method: "POST",
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(
+                        {
+                            nombre: nombre,
+                            precio: precio,
+                            categoria: categoria
+                        }
+                    )
+                })
+                if (!response.ok){
+                    throw new Error("Error en la solicitud")
+                }
+                const data = await response.json()
+                console.log(data)
+                break;
+            }else{
+                console.log("solicitud incompleta")
+                break;
             }
     }
 }
