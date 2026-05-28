4be12ef4c2d9ce579c1d1066704526fcdab92411
Gaston M <gastonux@gmail.com>
2026-05-28 02:01:34 -0300

    Se crea la url de la API con la que vamos a trabajar, se leen los argumentos con process.argv y se seleccionan los argumentos validos. Secrea el programa principal y mediante switc se selecciona cada argumento, se realiza para el case GET products

--- pre-entrega/index.js
+++ pre-entrega/index.js
@@ -1,1 +1,38 @@
 console.log("Iniciando el programa...")
+const url_API = "https://fakestoreapi.com"
+
+const argumentos = process.argv.slice(2)
+
+const argumentos_validos = ["GET", "POST", "PUT", "DELETE"]
+
+console.log(argumentos) 
+
+async function programa_principal(argumentos) {
+    if (!argumentos[0] in argumentos_validos){
+        console.log("Comando incorrecto")
+        return
+    }
+    switch (argumentos[0]){
+        case "GET":
+            if (!argumentos[1].includes("/") && argumentos[1] == "products"){
+                try{
+                    const response = await fetch(`${url_API}/products`, {
+                        method: "GET"
+                    })
+                    if (response.status !== 200){
+                        throw new Error("Falla en la solicitud")
+                        break
+                    }
+                    const data = await response.json()
+                    data.forEach(element => {
+                        console.log(element)
+                    })
+                }catch(error){
+                    console.log(error)
+                    break
+                }
+            }
+    }
+}
+
+programa_principal(argumentos)
